3cd24973e856a32e4c4b08ca4f22fb8468cd99ee
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-12-06 14:33:18 +0000

    Change drivers/power/supply from another kernel non hybris

--- drivers/power/supply/qcom/qg-util.c
+++ drivers/power/supply/qcom/qg-util.c
@@ -374,13 +374,7 @@ int qg_get_battery_temp(struct qpnp_qg *chip, int *temp)
 		pr_err("Failed reading BAT_TEMP over ADC rc=%d\n", rc);
 		return rc;
 	}
-
-#if defined(CONFIG_TCT_PM7250_COMMON)
-#if defined(DISABLE_TEMPERATURE_DETECTION_AND_THERMAL_POLICY)
-	pr_debug("temperature fixed at 25 degree! The real temp is=%d\n", *temp);
-	*temp = 250;
-#endif
-#endif
+	pr_debug("batt_temp = %d\n", *temp);
 
 	return 0;
 }
